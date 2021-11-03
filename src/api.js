520e138da044df32c9f617906d7c74fe09ba7bc1
Hilman Fajri Fahriansyah <hilmanfajri24@gmail.com>
2021-11-03 11:09:34 +0700

    update

--- src/api.js
+++ src/api.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const axios = require("axios");
 const serverless = require("serverless-http");
 
 const app = express();
 const router = express.Router();
 
-router.get("/:path", async (req, res) => {
+router.get("/:path", async function (req, res){
   var apiKey = '13cd61f5fd174979a8a0cc3c94b3628d'
-  var path = req.originalUrl
+  var path = req.originalUrl.replace('/.netlify/functions/api','')
 
   var url = 'https://newsapi.org/v2' + path + '&apiKey=' + apiKey
   var response = await axios.get(url)
